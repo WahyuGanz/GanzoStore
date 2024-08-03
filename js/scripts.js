d64bffaab6acc01a18b868d8e3f2d961a1cb77c5
Wahyu Ganzo <85389064+WahyuGanz@users.noreply.github.com>
2024-08-03 12:54:01 +0700

    UPDATE
    Penambahan Produk Baru!

--- js/scripts.js
+++ js/scripts.js
@@ -75,6 +75,156 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "Rp25.000.000",
             image: "images/product15.jpg"
         },
+        {
+            name: "Erigo Oversized T-Shirt",
+            description: "Rp150.000",
+            image: "images/product16.jpg"
+        },
+        {
+            name: "3Second Hoodie",
+            description: "Rp350.000",
+            image: "images/product17.jpg"
+        },
+        {
+            name: "Roughneck 1991 Denim Jacket",
+            description: "Rp500.000",
+            image: "images/product18.jpg"
+        },
+        {
+            name: "Brodo Derby Shoes",
+            description: "Rp700.000",
+            image: "images/product19.jpg"
+        },
+        {
+            name: "Eiger Adventure Backpack",
+            description: "Rp450.000",
+            image: "images/product20.jpg"
+        },
+        {
+            name: "Samsung Galaxy S24",
+            description: "Rp15.000.000",
+            image: "images/product21.jpg"
+        },
+        {
+            name: "Apple iPhone 15",
+            description: "Rp20.000.000",
+            image: "images/product22.jpg"
+        },
+        {
+            name: "Google Pixel 8",
+            description: "Rp12.000.000",
+            image: "images/product23.jpg"
+        },
+        {
+            name: "OnePlus 12",
+            description: "Rp10.000.000",
+            image: "images/product24.jpg"
+        },
+        {
+            name: "Xiaomi Mi 14",
+            description: "Rp8.000.000",
+            image: "images/product25.png"
+        },
+        {
+            name: "MacBook Pro 2024",
+            description: "Rp30.000.000",
+            image: "images/product26.jpg"
+        },
+        {
+            name: "Dell XPS 15 2024",
+            description: "Rp25.000.000",
+            image: "images/product27.jpg"
+        },
+        {
+            name: "HP Spectre x360 2024",
+            description: "Rp22.000.000",
+            image: "images/product28.jpg"
+        },
+        {
+            name: "Asus ROG Zephyrus G14 2024",
+            description: "Free",
+            image: "images/product29.png"
+        },
+        {
+            name: "Lenovo ThinkPad X1 Carbon 2024",
+            description: "Rp27.000.000",
+            image: "images/product30.jpg"
+        },
+        {
+            name: "LG UltraGear 27GN950",
+            description: "Rp10.000.000",
+            image: "images/product31.jpg"
+        },
+        {
+            name: "Samsung Odyssey G9",
+            description: "Rp20.000.000",
+            image: "images/product32.jpg"
+        },
+        {
+            name: "ASUS ROG Swift PG259QN",
+            description: "Rp15.000.000",
+            image: "images/product33.jpg"
+        },
+        {
+            name: "Dell Alienware AW3420DW",
+            description: "Rp18.000.000",
+            image: "images/product34.jpg"
+        },
+        {
+            name: "Acer Predator X27",
+            description: "Rp25.000.000",
+            image: "images/product35.jpg"
+        },
+        {
+            name: "DJI Mavic Air 2",
+            description: "Rp15.000.000",
+            image: "images/product36.jpg"
+        },
+        {
+            name: "DJI Phantom 4 Pro",
+            description: "Rp20.000.000",
+            image: "images/product37.jpg"
+        },
+        {
+            name: "Parrot Anafi",
+            description: "Rp10.000.000",
+            image: "images/product38.jpg"
+        },
+        {
+            name: "Autel Robotics EVO II",
+            description: "Rp18.000.000",
+            image: "images/product39.jpg"
+        },
+        {
+            name: "Skydio 2",
+            description: "Rp12.000.000",
+            image: "images/product40.jpg"
+        },
+        {
+            name: "Canon EOS R5",
+            description: "Rp50.000.000",
+            image: "images/product41.png"
+        },
+        {
+            name: "Nikon Z7 II",
+            description: "Rp45.000.000",
+            image: "images/product42.jpg"
+        },
+        {
+            name: "Sony Alpha 7R IV",
+            description: "Rp55.000.000",
+            image: "images/product43.png"
+        },
+        {
+            name: "Fujifilm X-T4",
+            description: "Rp30.000.000",
+            image: "images/product44.jpg"
+        },
+        {
+            name: "Panasonic Lumix S1R",
+            description: "Rp40.000.000",
+            image: "images/product45.jpg"
+        },
     ];
 
     const productList = document.getElementById('product-list');
